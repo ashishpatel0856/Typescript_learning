f701f234e65846f50edee0b046505c97e8f353a4
Ashish Patel <ashishkumarr0856@gmail.com>
2026-08-13 18:49:58 +0530

    add oops and array

--- basic/src/ArrayEnum.ts
+++ basic/src/ArrayEnum.ts
@@ -1,2 +1,82 @@
 const chaiFlavour: string[] = ["masala","Adrak"]
 const chaiPrice: number[] = [10,20]
+
+const rating: Array<number> = [4,6,2.1,7]
+
+type Chai = {
+    name: string;
+    price: number
+}
+
+const menu: Chai[] = [
+    {name: "masala", price: 15},
+    {name: "adrak", price:25},
+
+]
+
+const cities: readonly string[]= ["delhi","jaipur"]
+
+
+const table: number[][] = [
+    [1,2,3],
+    [4,8,25]
+]
+
+let chaitype: [string,number];
+chaitype = ["masala",20]
+// chaitype= [20,[masala]]
+
+let userinfo: [string,number,boolean?]
+userinfo = ["ashish",100]
+userinfo = ["ashish",100,true]
+
+
+const location: readonly [number,number] = [23,2.1]
+
+const chaiItems: [name: string, price: number] = ["masala",50]
+
+// enum
+enum CupSize {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
+
+const size = CupSize.LARGE;
+
+enum Status {
+    PENDING =100,
+    SERVERD, // 101
+    CANCELLED //102
+}
+
+enum ChaiType {
+    MASALA = "masala",
+    GINGER = "ginger"
+}
+
+
+function makeChai(type: ChaiType) {
+    console.log(`making: ${type}`);
+}
+
+makeChai(ChaiType.GINGER);
+// makeChai("masala");
+
+enum RandomEnum {
+    ID =1,
+    NAME = "chai"
+}
+
+
+const enum sugar {
+    LOW =1,
+    MEDIUM =2,
+    HIGH =3
+}
+
+const s = sugar.HIGH;
+
+
+let t: [string,number] = ["chai",10]
+t.push("extra")
---
+++ basic/src/oops.ts
@@ -0,0 +1,86 @@
+// class Chai {
+//     flavour: string;
+//     price: number
+
+//     constructor(flavour: string, price: number){
+//         this.flavour = flavour
+//         this.price = price
+//         console.log(this)
+//     }
+// }
+
+// const masalaChai = new Chai("Ginger",20);
+// masalaChai.flavour = "masala"
+
+
+class Chai {
+    public flavor: string = "masala"
+    private secretIngredients = "Cardamom"
+
+    reveal() {
+        return this.secretIngredients // ok
+    }
+}
+
+
+
+
+class shop {
+        protected shopName = "Chai corner"
+
+}
+class Branch extends shop {
+    getName(){
+        return this.shopName
+    }
+}
+
+
+
+class walet {
+    #balance = 100
+    getBalance() {
+        return this.#balance
+    }
+
+}
+const w = new walet();
+
+class Cup{
+    readonly capacity: number = 250
+
+    constructor(capacity:number){
+        this.capacity = capacity
+    }
+}
+
+class ModernChai  {
+    private _sugar = 2
+    get sugar() {
+        return this._sugar
+    }
+    set sugar(value:number){
+        if(value >5) throw new Error("Too sweet");
+        this._sugar=value
+    }
+}
+
+const c = new ModernChai();
+c.sugar =3
+
+class Ekchai {
+    static shopname ='chaicode caffe'
+    constructor(public flavour: string){}
+}
+console.log(Ekchai.shopname);
+
+abstract class Drink{
+    abstract make(): void
+}
+
+
+class mychai extends Drink{
+    make() {
+        console.log("brewing chai");
+    }
+}
